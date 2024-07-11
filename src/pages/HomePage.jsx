aed98574e02cdd07d4657a738b534f070ceaaf58
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-07-11 14:54:53 +0200

    finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,28 +8,30 @@ function HomePage() {
   const [students, setStudents] = useState(studentsData);
 
   return (
-    <Link to="/students/:studentId">
-      <div className="border-2 border-rose-500 m-2">
-        <h1>Home Page</h1>
-        <div className="flex justify-between items-center p-2 font-bold border-b w-full">
-          <span
-            className="flex items-center justify-center"
-            style={{ flexBasis: "20%" }}
-          >
-            Image
-          </span>
-          <span style={{ flexBasis: "20%" }}>Name</span>
-          <span style={{ flexBasis: "20%" }}>Program</span>
-          <span style={{ flexBasis: "20%" }}>Email</span>
-          <span style={{ flexBasis: "20%" }}>Phone</span>
-        </div>
-
-        {students &&
-          students.map((student) => {
-            return <StudentCard key={student._id} {...student} />;
-          })}
+    <div className="border-2 border-rose-500 m-2">
+      <h1>Home Page</h1>
+      <div className="flex justify-between items-center p-2 font-bold border-b w-full">
+        <span
+          className="flex items-center justify-center"
+          style={{ flexBasis: "20%" }}
+        >
+          Image
+        </span>
+        <span style={{ flexBasis: "20%" }}>Name</span>
+        <span style={{ flexBasis: "20%" }}>Program</span>
+        <span style={{ flexBasis: "20%" }}>Email</span>
+        <span style={{ flexBasis: "20%" }}>Phone</span>
       </div>
-    </Link>
+
+      {students &&
+        students.map((student, index) => {
+          return (
+            <Link key={index} to={`/students/${student._id}`}>
+              <StudentCard key={student._id} {...student} />
+            </Link>
+          );
+        })}
+    </div>
   );
 }
 
